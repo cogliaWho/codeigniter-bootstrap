22a24949427a3515a307b0d771b3cf3857155ff3
cogliaWho <coglia@gmail.com>
2015-12-01 18:51:37 +0100

    removed index.php from URL and added reference to css and js files in header and footer

--- application/views/templates/footer.php
+++ application/views/templates/footer.php
@@ -1,3 +1,5 @@
 		<em>&copy; 2015</em>
     </body>
+	<script async="" type="text/javascript" src="<?php echo site_url('resources/js/jquery.js'); ?>"></script>
+	<script async="" type="text/javascript" src="<?php echo site_url('resources/js/main.js'); ?>"></script>
 </html>
